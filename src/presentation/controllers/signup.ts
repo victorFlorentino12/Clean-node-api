61f5c801b3ab855610b6b77eb8945709d91524ed
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-07 15:46:30 -0300

    feat: ensure SingUpController return an erro if no name provide

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@ export class SingUpController {
   hundle (_httpRequest: any): any {
     console.log(_httpRequest)
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     }
   }
 }
