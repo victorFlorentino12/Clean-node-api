a68fa9bde6522f954201337647eca98206e7559d
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-07 01:32:04 -0300

    test: ensure SingUpController return statusCode 440 if no provide name

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SingUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Shold return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        email: 'florentino@email.com',
+        password: '12345',
+        passwordConfirmation: '12345'
+      }
+    }
+    const httpResponse = sut.hundle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
