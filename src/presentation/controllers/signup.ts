c37f662d5135aad0677ca47bf9ecf38a45076982
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-07 22:22:09 -0300

    feat: ensure SingUpController if no email or mame is provider

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,16 @@
 export class SingUpController {
   hundle (_httpRequest: any): any {
-    console.log(_httpRequest)
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!_httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+    if (!_httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
