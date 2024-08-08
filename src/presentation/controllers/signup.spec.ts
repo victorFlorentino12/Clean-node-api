ce86964ffa3df18bdaedae4f3ffebca304131c11
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-07 22:50:55 -0300

    test: ensure SignUpController correct interface in response in the request

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,16 +1,17 @@
 import { SingUpController } from './signup'
+import type { httpRequest, httpResponse } from '../protocols/http'
 
 describe('SignUp Controller', () => {
   test('Shold return 400 if no name is provided', () => {
     const sut = new SingUpController()
-    const httpRequest = {
+    const httpRequest: httpRequest = {
       body: {
         email: 'florentino@email.com',
         password: '12345',
         passwordConfirmation: '12345'
       }
     }
-    const httpResponse = sut.hundle(httpRequest)
+    const httpResponse: httpResponse = sut.hundle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
@@ -25,7 +26,7 @@ describe('singUp Controller', () => {
         passwordConfirmation: '12345'
       }
     }
-    const httpResponse = sut.hundle(httpRequest)
+    const httpResponse: httpResponse = sut.hundle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
