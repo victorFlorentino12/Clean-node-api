26642c70ca8d61ffcd7bd3d1376ca99a242c5aef
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-08 14:36:49 -0300

    feat: adding new param to singUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { ErrorMissingParam } from '../error/error-missing-params'
 import { badRequest } from '../helpers/http-helper'
 export class SingUpController {
   hundle (_httpRequest: httpRequest): httpResponse {
-    const params = ['name', 'email', 'password']
+    const params = ['name', 'email', 'password', 'passwordConfirmation']
     for (const value of params) {
       if (!_httpRequest.body[value]) {
         return badRequest(new ErrorMissingParam(value))
