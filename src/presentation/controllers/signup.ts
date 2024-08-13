fd5339e48e3e35d6934b935a53ca7468f5243fc1
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-08-13 18:12:48 -0300

    feat: ensure SingUpController calls addAcount with values corrects

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,15 @@
+import type { AddAccount } from '../../domain/usercase/add-account'
 import { ErrorMissingParam, ErrorInvalidParam, PasswordDifferentError } from '../error/index'
 import { badRequest, serverError } from '../helpers/http-helper'
 import type { Controller, EmailValidator, httpRequest, httpResponse } from '../protocols/index'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   hundle (_httpRequest: httpRequest): httpResponse {
@@ -22,9 +25,15 @@ export class SingUpController implements Controller {
       if (!isValid) {
         return badRequest(new ErrorInvalidParam('email'))
       }
-      if (_httpRequest.body.password !== _httpRequest.body.passwordConfirmation) {
+      const { name, email, password, passwordConfirmation } = _httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new PasswordDifferentError())
       }
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
